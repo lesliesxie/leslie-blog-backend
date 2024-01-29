f37612e2e731fbec132a6d800bf6ace6f49cda50
xiaoying xie <xiaoyingxie@deepglint.com>
2024-01-29 18:58:06 +0800

    format database.module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,23 +3,23 @@
  * @Author: leslie
  * @Date: 2024-01-26 19:10:40
  * @LastEditors: leslie
- * @LastEditTime: 2024-01-28 21:33:12
+ * @LastEditTime: 2024-01-29 17:52:00
  * 佛祖保佑没bug
  */
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
+      type: 'mysql',
+      host: 'localhost',
       port: 3306,
-      username: "root",
-      password: "072745h.",
-      database: "leslie",
+      username: 'root',
+      password: '072745h.',
+      database: 'leslie',
       entities: [],
       synchronize: true,
-    })
-  ]
+    }),
+  ],
 })
 export class DatabaseModule {}
